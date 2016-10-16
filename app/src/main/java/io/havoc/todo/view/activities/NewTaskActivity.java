f86bea60cc4999982027d509b1a42031a4c2d8df
nxt3 <nategantt@gmail.com>
2016-10-16 15:41:23 -0400

    Completed basic UI for new task screen; only doing Name, Description, and Priority for now

--- app/src/main/java/io/havoc/todo/view/activities/NewTaskActivity.java
+++ app/src/main/java/io/havoc/todo/view/activities/NewTaskActivity.java
@@ -2,14 +2,28 @@ package io.havoc.todo.view.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.havoc.todo.R;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.md_light_blue_600));
+
+        toolbar.setNavigationOnClickListener(view -> this.finish());
     }
 }
