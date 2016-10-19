72b76c9028e5aaca6e1cbc9a523382a96c1adc73
nxt3 <nategantt@gmail.com>
2016-10-19 14:44:46 -0400

    Setting DefaultNightMode now happens in the Application class

--- app/src/main/java/io/havoc/todo/Havoc.java
+++ app/src/main/java/io/havoc/todo/Havoc.java
@@ -1,11 +1,16 @@
 package io.havoc.todo;
 
 import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
 
 import io.havoc.todo.model.service.HavocService;
 
 
 public class Havoc extends Application {
+
+    static {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+    }
 
     @Override
     public void onCreate() {
--- app/src/main/java/io/havoc/todo/view/activities/MainActivity.java
+++ app/src/main/java/io/havoc/todo/view/activities/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 
 import net.grandcentrix.thirtyinch.TiActivity;
@@ -54,8 +53,6 @@ public class MainActivity extends TiActivity<MainActivityPresenter, MainActivity
         ButterKnife.bind(this);
 
         fabNewTask.setOnClickListener(view -> getPresenter().newTaskButtonClicked());
-
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
 
 //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
